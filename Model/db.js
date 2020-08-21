c866c58804c43ca446281a49ab878bd540b18b23
NanduKkd <nandukkd7164@gmail.com>
2020-08-21 20:20:44 +0530

    db signIn fix

--- Model/db.js
+++ Model/db.js
@@ -3,7 +3,7 @@ import firestore from "@react-native-firebase/firestore";
 const db = firestore();
 
 export async function signUp(username, password){
-	if( (await db.collection("users").where("username", "==", username).get()).docs.map(doc => doc.data()).length>0){
+	if( (await db.collection("users").where("username", "==", username).get()).docs.length>0){
 		return false;
 	}
 	 doc = await db.collection("users").add({username, password});
@@ -11,11 +11,12 @@ export async function signUp(username, password){
 }
 
 export async function signIn(username, password){
-	const docs = (await db.collection("users").where("username", "==", username)).docs?.map(doc => doc.data());
+	const docs = (await db.collection("users").where("username", "==", username).get()).docs?.map(doc => ({id:doc.id, ...doc.data()}));
 	if(docs && docs.length>0){
 		if(docs[0].password===password){
 			return docs[0].id;
 		}
+		return false;
 	}
 	return false;
 }
